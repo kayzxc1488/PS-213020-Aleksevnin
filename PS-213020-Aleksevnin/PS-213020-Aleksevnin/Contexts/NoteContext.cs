df177b15eb87bf57a3e2b9e08f753291535dc116
kayzxc1488 <lilfenix1488@yandex.ru>
2021-10-02 17:55:20 +0300

    Что-то сделали

---
+++ PS-213020-Aleksevnin/PS-213020-Aleksevnin/Contexts/NoteContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PS_213020_Aleksevnin.Models;
+
+namespace PS_213020_Aleksevnin.Contexts
+{
+    public class NoteContext : DbContext
+    {
+        public NoteContext(DbContextOptions<NoteContext> option) : base(option)
+        {
+            
+        }
+        public DbSet<Note> Notes { get; set; }
+
+    }
+}
